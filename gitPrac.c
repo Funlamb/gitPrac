36143a8a5a971d7c5857596a63682357e9873bfe
Funlamb <funlamb@gmail.com>
2021-10-05 16:59:41 -0700

    Trying to get an int into a char*

--- gitPrac.c
+++ gitPrac.c
@@ -1,7 +1,18 @@
-//trying to get git to work
 #include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#define atoa(x) #x
+
+char* intBeautify(int a);
 
 int main(void) {
+    setlocale(LC_ALL, "");
+
+    const struct lconv * const currentlocale = localeconv();
+
+    printf("In the current locale, the default currency symbol is: %s\n",
+        currentlocale->currency_symbol);
+
     int outstandingShares = 76500000;
     int csAccounts;
     int GMEaccounts;
@@ -15,17 +26,42 @@ int main(void) {
     printf("Hello apes,\n");
     printf("You can call me the average guy.\n");
     printf("With %d accounts at ComputerShare it will take:\n", csAccounts);
-    printf("76.5M / 349,000 = %.3f Shares (All Outstanding Shares)\n", allOutstandingShare); 
-    printf("67.5M / 349,000 = %.3f Shares (All Outstanding Shares - RC’s shares)\n", aosMinusRC);
-    printf("40.1M / 349,000 = %.3f Shares (All Outstanding Shares - Institutional ownership) ", aosMinusInstitutional);
+    printf("76.5M / %d = %.3f Shares (All Outstanding Shares)\n", GMEaccounts, allOutstandingShare); 
+    printf("67.5M / %d = %.3f Shares (All Outstanding Shares - RC’s shares)\n", GMEaccounts, aosMinusRC);
+    printf("40.1M / %d = %.3f Shares (All Outstanding Shares - Institutional ownership) ",GMEaccounts, aosMinusInstitutional);
     printf("These numbers are estimates. Don’t forget to do your part and DRS your shares.\n\n");
     printf("New apes count calculated from today's high score - the first account number I saw when we first started counting CS accounts.\n\n");
     printf("%d - 30,000 = %d (New CS accounts - previous accounts)\n", csAccounts, GMEaccounts);
-    
+    printf("%s\n", intBeautify(GMEaccounts));
     return 0;
-//It looks like I have fixed everything in this file. 
-//I have some more work to do with the purchase of one share of AMC
-//I ordered the share and told Fedility to DRS the share as soon as it settles. 
-//Trying out git restore
-//Another test becuase this part is done from home. 
+//Need to beautify the numbers
+}
+
+char* intBeautify(int a){
+    //find the number of digits in the int
+    int ENOUGH = 10;
+    char* string;//[ENOUGH];
+    sprintf(string, "%i", a);
+    // int tempNum = a;
+    // int intCount = 0;
+    // while (a>0){
+    //     tempNum = tempNum % 10;
+    //     printf("%i", tempNum);
+    //     intCount++;
+    // }
+
+    // printf("%d", intCount);
+    //create a char* of length + ,s
+    //formate the char*
+
+    // setlocale(LC_ALL, "");
+    // const struct lconv * const currentlocale = localeconv();
+    // printf("In the current locale, the default currency symbol is: %s\n",
+    //     currentlocale->currency_symbol);
+    // std::cout.imbue(std::locale(""));
+    // std::cout << 123456789 << "\n";
+    // char *string = atoa(a);
+    // char* intToString;
+    // sprintf(string, "%i", a);
+    return string;
 }
